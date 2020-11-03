165c08a9f7aafcba263e48f8965e4dd0a8857999
Jason McGinty <jason_mcginty@yahoo.com>
2020-11-02 20:21:41 -0600

    adding heroku postgresql database to environment variables

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'yeet')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///solar_system'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///solar_system')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
